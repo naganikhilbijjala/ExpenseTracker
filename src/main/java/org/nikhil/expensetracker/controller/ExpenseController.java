43e1885b86f4deffc7668c3982b62a28fb3d3c84
Naga Nikhil Bijjala <naganikhil.bijjala@gmail.com>
2024-01-22 12:30:39 -0500

    add timestamp and response status code to APIs

--- src/main/java/org/nikhil/expensetracker/controller/ExpenseController.java
+++ src/main/java/org/nikhil/expensetracker/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package org.nikhil.expensetracker.controller;
 import org.nikhil.expensetracker.entity.Expense;
 import org.nikhil.expensetracker.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,11 +22,13 @@ public class ExpenseController {
         return expenseService.getExpenseById(id);
     }
 
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping("/expenses")
     public void deleteExpenseById(@RequestParam Long id){
         expenseService.deleteExpenseById(id);
     }
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/expenses")
     public Expense saveExpenseDetails(@RequestBody Expense expense){
         return expenseService.saveExpenseDetails(expense);
